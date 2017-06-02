a39ad1a195d08ae322ad79cd36e592d594f17a87
Luis Lopez <luislodero@gmail.com>
2017-06-02 13:52:33 -0500

    Image has been uploaded

---
+++ GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  VideoGameCollector
+//
+//  Created by Luis Lopez on 6/2/17.
+//  Copyright © 2017 Luis Lopez. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTabbed(_ sender: Any) {
+    }
+   
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
